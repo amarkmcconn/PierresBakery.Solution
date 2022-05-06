6bf4e822e3514594bd2367ca39a1b7a2f9e79409
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-06 13:55:29 -0700

    1st Test written Pastry.

---
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using PierresBakery;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newBread.GetType());
+    }
+  }
+}
---
+++ PierresBakery/Models/Pastry.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresBakery
+{
+  public class Pastry
+  {
+    // public int PastriesAmount { get; set; }
+    // public int PastryOrderTotal { get; set; }
+    // public Pastry  (int pastryAmount)
+    // {
+    //   PastriesAmount = pastryAmount;
+    //   PastryOrderTotal = 0;
+    // }
+  }
+} 
