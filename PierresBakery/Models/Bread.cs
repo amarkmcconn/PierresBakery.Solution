40f71c6fe0c18a12121f22c9cd5f709c0b9c7331
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-06 10:42:02 -0700

    Test 1 good pass.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -5,6 +5,12 @@ namespace PierresBakery
 {
   public class Bread
   {
-
+    public int BreadPrice { get; set;}
+    public int BreadBaugettes { get; set;}
+    public Bread (int breadPrice, int breadBaugettes)
+    {
+      BreadPrice = 5;
+      BreadBaugettes = breadBaugettes;
+    }
   }
 }
