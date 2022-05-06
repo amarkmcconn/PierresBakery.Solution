acbe757fb23e43219fcc690bfc4bdfb49affc7ef
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-06 09:58:34 -0700

    logic creates empty item.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -5,16 +5,6 @@ namespace PierresBakery
 {
   public class Bread
   {
-    
-    public int OrderNum {get; set;}
 
-    public Bread ( int amount)
-    {
-      OrderNum = amount;
-    }
-    public int OrderTotal ()
-    {
-      return 0;
-    }
   }
 }
