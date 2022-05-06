12c5456918ea25eac00a87e0fdc192ddadf2955a
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-06 09:03:36 -0700

    first test written.

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -1,16 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using PierresBakery;
 
-namespace ProjectName.Tests
+namespace PierresBakery.Tests
 {
   [TestClass]
-  public class ClassNameTests
+  public class BreadTests
   {
     [TestMethod]
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void OrderTotal_GivesPriceForOrder_Int()
     {
-      // any necessary logic to prep for test; instantiating new classes, etc.
-      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+      Bread inputBread = new Bread (Baugette, 5);
+      Assert.AreEqual(5, BreadPrice);
     }
   }
 }
